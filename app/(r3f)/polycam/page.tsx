b799215b9b9ae45701940b93a6de3421a0a88b4a
mholovko <mykola@spatialtech.info>
2023-10-09 15:31:31 +0200

    fix: update polycam Demo

--- app/(r3f)/polycam/page.tsx
+++ app/(r3f)/polycam/page.tsx
@@ -29,6 +29,7 @@ export default async function IndexPage() {
               <p className="max-w-[700px] text-lg text-muted-foreground">
                 Recently, Polycam released
                 <Link
+                  target="blank"
                   className="underline"
                   href={"https://poly.cam/gaussian-splatting"}
                 >
@@ -41,7 +42,18 @@ export default async function IndexPage() {
               </div>
             </div>
           </div>
-          <div className="aspect-video w-full bg-slate-700">DEMO VIDEO</div>
+          <div className="relative aspect-auto h-full overflow-hidden rounded-sm bg-slate-700 lg:aspect-[4/3] lg:rounded-lg">
+            <video
+              className=" h-full w-full scale-[1.01] rounded-lg object-cover object-top"
+              autoPlay
+              loop
+              muted
+              playsInline
+              poster="/polycam_demo.png"
+            >
+              <source src="/polycam_demo.mp4" type="video/mp4" />
+            </video>
+          </div>
         </div>
 
         <h2 className="py-8 text-xl font-medium ">
@@ -51,10 +63,21 @@ export default async function IndexPage() {
         <div className="grid grid-cols-2 gap-4 py-2 md:grid-cols-4 md:gap-4 md:py-4">
           {highlights.map((item, index) => (
             <div key={item.id}>
-              <Link
-                className=" relative w-full "
-                href={`polycam/${item.objectID}`}
-              >
+              <div className="relative">
+                <Link
+                  className=" relative block h-full "
+                  href={`polycam/${item.objectID}`}
+                >
+                  <Image
+                    className="reliative aspect-[4/3] rounded-sm shadow hover:shadow-lg"
+                    src={item.thumbnail}
+                    width={512}
+                    height={512}
+                    alt={item.name}
+                    layout="responsive"
+                    style={{ objectFit: "cover" }}
+                  />
+                </Link>
                 <svg
                   className="absolute  bottom-0 right-0 z-10 h-8 w-8"
                   viewBox="0 0 32 32"
@@ -66,16 +89,7 @@ export default async function IndexPage() {
                     fill="currentColor"
                   ></path>
                 </svg>
-                <Image
-                  className="aspect-[4/3] rounded-sm shadow hover:shadow-lg"
-                  src={item.thumbnail}
-                  width={512}
-                  height={512}
-                  alt={item.name}
-                  layout="responsive"
-                  style={{ objectFit: "cover" }}
-                />
-              </Link>
+              </div>
               <p className="truncate pt-2 font-medium">
                 {item.name ? `${item.name}` : "Gaussian Splat"}
               </p>
